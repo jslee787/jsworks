b3f6e3c22da4e97c50a762f5b1f2b15301296e80
jslee787 <wltntodrkr@naver.com>
2021-11-08 12:47:18 +0900

    day01

---
+++ day01/js/hello.js
@@ -0,0 +1,7 @@
+//한 줄 주석 처리는 '//', 여러 줄 주석 처리는 '/* ~ */'
+//웹 문서 본문 출력
+document.write("hello~ world<br>")
+document.write("즐거운 웹 프로그래밍 !!")
+        
+//알림창 띄우기 window. 생략 가능
+window.alert("안녕하세요~");    
