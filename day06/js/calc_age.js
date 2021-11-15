431a9d9fcdf7415c857a9f72cdae65fd57d56894
jslee787 <wltntodrkr@naver.com>
2021-11-15 12:50:42 +0900

    day06

---
+++ day06/js/calc_age.js
@@ -0,0 +1,12 @@
+//나이 계산 프로그램
+function calcAge(){
+    //현재, 출생년도 입력
+    const CURRENT_YEAR = 2021;
+    var birthYear = prompt("태어난 해를 입력하세요.");
+    var age;
+    //계산
+    age = CURRENT_YEAR - birthYear + 1;
+
+    //출력
+    document.getElementById("result").innerHTML = "나이는 " + age + "세 입니다.";
+}
